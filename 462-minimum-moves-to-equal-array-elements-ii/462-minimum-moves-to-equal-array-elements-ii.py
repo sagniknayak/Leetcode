1cf98074f7cb7f89347086dfa38607a66eb7d2a4
Sagnik Nayak <sagniknayak26@gmail.com>
2022-06-30 11:12:26 +0530

    Time: 107 ms (57.22%), Space: 15.5 MB (33.43%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.py
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.py
@@ -2,7 +2,5 @@ class Solution:
     def minMoves2(self, nums: List[int]) -> int:
         nums.sort()
         val = nums[len(nums)//2]
-        moves = 0
-        for item in nums:
-            moves += abs(item-val)
+        moves = sum(list(map(lambda x: abs(x-val), nums)))
         return moves
