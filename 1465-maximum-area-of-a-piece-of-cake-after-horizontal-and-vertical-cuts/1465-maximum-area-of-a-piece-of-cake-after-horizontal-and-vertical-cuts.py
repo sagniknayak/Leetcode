ef434fe8b03eff6fa36b7cdf9f2bc542a8e3828f
Sagnik Nayak <sagniknayak26@gmail.com>
2022-07-02 10:01:32 +0530

    Time: 336 ms (93.37%), Space: 28.6 MB (24.68%) - LeetHub

--- 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.py
+++ 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.py
@@ -4,18 +4,5 @@ class Solution:
         verticalCuts.sort()
         h_cuts = [0]+horizontalCuts+[h]
         v_cuts = [0]+verticalCuts+[w]
-        max_h = 0
-        for i in range(len(h_cuts)):
-            h_val = h_cuts[i]-h_cuts[i-1]
-            if h_val > max_h:
-                max_h = h_val
-        max_v = 0
-        for i in range(len(v_cuts)):
-            v_val = v_cuts[i]-v_cuts[i-1]
-            if v_val > max_v:
-                max_v = v_val
-                
-        print(f'max h {max_h}  max v {max_v}')
-        print(f'val {max_h*max_v}')
-                
-        return int((max_h*max_v) % (1000000007))
+        return int((max([h_cuts[i]-h_cuts[i-1] for i in range(len(h_cuts))])*max([v_cuts[i]-v_cuts[i-1] for i in range(len(v_cuts))]))%(1000000007))
+        
