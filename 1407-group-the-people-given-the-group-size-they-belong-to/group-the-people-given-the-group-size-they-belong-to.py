00c3bb1e3bededfb8c2c4cdd61d1b941c6d98890
Sagnik Nayak <sagniknayak26@gmail.com>
2023-09-11 09:57:24 +0530

    Time: 73 ms (69.53%) | Memory: 16.6 MB (31.58%) - LeetSync

--- 1407-group-the-people-given-the-group-size-they-belong-to/group-the-people-given-the-group-size-they-belong-to.py
+++ 1407-group-the-people-given-the-group-size-they-belong-to/group-the-people-given-the-group-size-they-belong-to.py
@@ -1,12 +1,17 @@
 class Solution:
     def groupThePeople(self, groupSizes: List[int]) -> List[List[int]]:
         group = dict()
-        for i in range(len(groupSizes)):
-            group[groupSizes[i]] = group.get(groupSizes[i],[])+[i] 
         groups = list()
-        for key in group.keys():
-            members = group[key]
-            for i in range(0,len(members),key):
-                groups.append(members[i:key+i])
+        for i in range(len(groupSizes)):
+            group[groupSizes[i]] = group.get(groupSizes[i],[])+[i]
+            if len(group[groupSizes[i]]) == groupSizes[i]:
+                groups.append(group[groupSizes[i]])
+                group[groupSizes[i]] = []
+        print(groups)
+        
+        # for key in group.keys():
+        #     members = group[key]
+        #     for i in range(0,len(members),key):
+        #         groups.append(members[i:key+i])
         return groups
         
