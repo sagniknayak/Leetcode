b1686e07605199ef6d5c415c54b956ab73b52f74
Sagnik Nayak <sagniknayak26@gmail.com>
2024-03-26 16:27:01 +0530

    Time: 310 ms (26.47%) | Memory: 41.5 MB (5.31%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.py
+++ 41-first-missing-positive/first-missing-positive.py
@@ -1,10 +1,11 @@
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
-        seq = OrderedDict([(i,True) for i in range(1,len(nums)+2)])
-        for key in nums:
-            try:
-                del seq[key]
-            except:
+        seq = OrderedDict([(i, True) for i in nums])
+        for check in range(1,len(nums)+2):
+            if seq.get(check, False):
                 pass
-
-        return next(iter(seq.keys()))
+            else:
+                return check
+    
+        
+        
