17e8e8116fb2daed0364eafada67830e8bae4f69
Sagnik Nayak <sagniknayak26@gmail.com>
2025-02-10 13:04:47 +0530

    Time: 2359 ms (25.19%) | Memory: 302.2 MB (6.01%) - LeetSync

---
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -0,0 +1,25 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        palin = dict()
+        leng = len(s)
+        palin[1] = [((i,i),s[i]) for i in range(leng)]
+        palin[2] = list(filter(lambda x: x[1][0] == x[1][1],[((i,i+1),s[i]+s[i+1]) for i in range(leng-1)]))
+        for i in range(3, leng+1):
+            if palin[i-1] == palin[i-2]:
+                break
+            if palin[i-2] == []:
+                palin[i] = []
+                continue
+            bases = list(filter(lambda x: x[0][0]-1>=0 and x[0][1]+1<leng,palin[i-2]))
+            new_palins = list()
+            for base in bases:
+                if s[base[0][0]-1] == s[base[0][1]+1]:
+                    new_palins.append(((base[0][0]-1,base[0][1]+1),s[base[0][0]-1]+base[1]+s[base[0][1]+1]))
+            palin[i] = new_palins
+        for i in range(leng,0,-1):
+            if palin.get(i,[]) == []:
+                continue
+            else:
+                return palin[i][0][1]
+        return ""
+        
