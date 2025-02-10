3eba79409c68f0a2f39efe4a2078b1441a96ca9c
Sagnik Nayak <sagniknayak26@gmail.com>
2025-02-10 13:13:55 +0530

    Time: 4365 ms (14.81%) | Memory: 306.6 MB (6.01%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -5,18 +5,16 @@ class Solution:
         palin[1] = [((i,i),s[i]) for i in range(leng)]
         palin[2] = list(filter(lambda x: x[1][0] == x[1][1],[((i,i+1),s[i]+s[i+1]) for i in range(leng-1)]))
         for i in range(3, leng+1):
+            palin[i] = []
             if palin[i-1] == palin[i-2]:
                 break
             if palin[i-2] == []:
                 palin[i] = []
                 continue
-            bases = list(filter(lambda x: x[0][0]-1>=0 and x[0][1]+1<leng,palin[i-2]))
-            new_palins = list()
-            for base in bases:
+            for base in list(filter(lambda x: x[0][0]-1>=0 and x[0][1]+1<leng,palin[i-2])):
                 if s[base[0][0]-1] == s[base[0][1]+1]:
-                    new_palins.append(((base[0][0]-1,base[0][1]+1),s[base[0][0]-1]+base[1]+s[base[0][1]+1]))
-            palin[i] = new_palins
-        for i in range(leng,0,-1):
+                    palin[i] = palin[i]+[((base[0][0]-1,base[0][1]+1),s[base[0][0]-1]+base[1]+s[base[0][1]+1])]
+        for i in range(max(palin.keys()),0,-1):
             if palin.get(i,[]) == []:
                 continue
             else:
