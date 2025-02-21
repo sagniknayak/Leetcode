9e731a99b96bda5cbdc5f41c3760ed5f4323a9f1
Sagnik Nayak <sagniknayak26@gmail.com>
2025-02-21 16:43:02 +0530

    Time: 7 ms (59.54%) | Memory: 18.9 MB (61.40%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.py
+++ 735-asteroid-collision/asteroid-collision.py
@@ -1,25 +1,15 @@
 class Solution:
     def asteroidCollision(self, asteroids: List[int]) -> List[int]:
-        stable = [asteroids[0]]
-        for i in range(1, len(asteroids)):
-            asteroid = asteroids[i]
-            if asteroid < 0:
-                stable = self.collide(stable, asteroid)
+        stable = list()
+        for asteroid in asteroids:
+            while stable and asteroid < 0 < stable[-1]:
+                if stable[-1] < -asteroid:
+                    stable.pop()
+                    continue
+                elif stable[-1] == -asteroid:
+                    stable.pop()
+                break
             else:
                 stable.append(asteroid)
         return stable
-
-    def collide(self, stable, asteroid):
-        asteroid_size = abs(asteroid)
-        for i in range(len(stable)-1, -1, -1):
-            if stable[i] < 0:
-                return stable[:i+1] + [asteroid]
-            elif stable[i] > asteroid_size:
-                return stable[:i+1]
-            elif stable[i] == asteroid_size:
-                return stable[:i]
-            else:
-                pass
-        return [asteroid]
                 
-        return stable
